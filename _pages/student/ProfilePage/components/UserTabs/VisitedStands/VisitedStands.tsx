372f7e04a74bbe9bc004dce1268396e3e7008e9d
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2023-11-16 23:39:58 +0000

    fix(ProfilePage/components/tabs): undefined property "class"

--- _pages/student/ProfilePage/components/UserTabs/VisitedStands/VisitedStands.tsx
+++ _pages/student/ProfilePage/components/UserTabs/VisitedStands/VisitedStands.tsx
@@ -8,9 +8,9 @@ const stands: StandData[] = [
     img: 'https://play-lh.googleusercontent.com/8_XBPWRyDYWFVyVBGJYKU_5d5hxZGCYeh4SUhGpcRCaz_PE8GMSA70I9wF-qA6DJxGg=s256',
   },
   {
-    name: 'CelFocus',
+    name: 'NOS',
     desc: 'Empresa portuguesa de venda a retalho de produtos eletrónicos, de consumo e de entretenimento, pertencente ao grupo Sonae, com sede em Carnaxide.',
-    img: 'https://scontent.flis6-2.fna.fbcdn.net/v/t39.30808-6/305642484_603766534498182_5905359337023253547_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-TUe7M3U4AIAX-UTaqy&_nc_ht=scontent.flis6-2.fna&oh=00_AfA8y587sE4yryXhvic9a3aDOcXvOruccUZaxT_LQkHTIQ&oe=655542A3',
+    img: 'https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/102015/noslogotipo.png?itok=9kmnetW7',
   },
   {
     name: 'Accenture',
@@ -23,9 +23,9 @@ const stands: StandData[] = [
     img: 'https://play-lh.googleusercontent.com/8_XBPWRyDYWFVyVBGJYKU_5d5hxZGCYeh4SUhGpcRCaz_PE8GMSA70I9wF-qA6DJxGg=s256',
   },
   {
-    name: 'CelFocus',
+    name: 'NOS',
     desc: 'Empresa portuguesa de venda a retalho de produtos eletrónicos, de consumo e de entretenimento, pertencente ao grupo Sonae, com sede em Carnaxide.',
-    img: 'https://scontent.flis6-2.fna.fbcdn.net/v/t39.30808-6/305642484_603766534498182_5905359337023253547_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-TUe7M3U4AIAX-UTaqy&_nc_ht=scontent.flis6-2.fna&oh=00_AfA8y587sE4yryXhvic9a3aDOcXvOruccUZaxT_LQkHTIQ&oe=655542A3',
+    img: 'https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/102015/noslogotipo.png?itok=9kmnetW7',
   },
   {
     name: 'Accenture',
@@ -33,9 +33,9 @@ const stands: StandData[] = [
     img: 'https://www.careersinafrica.com/wp-content/uploads/2020/05/Accenture-logo-square.jpg',
   },
   {
-    name: 'CelFocus',
+    name: 'NOS',
     desc: 'Empresa portuguesa de venda a retalho de produtos eletrónicos, de consumo e de entretenimento, pertencente ao grupo Sonae, com sede em Carnaxide.',
-    img: 'https://scontent.flis6-2.fna.fbcdn.net/v/t39.30808-6/305642484_603766534498182_5905359337023253547_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-TUe7M3U4AIAX-UTaqy&_nc_ht=scontent.flis6-2.fna&oh=00_AfA8y587sE4yryXhvic9a3aDOcXvOruccUZaxT_LQkHTIQ&oe=655542A3',
+    img: 'https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/102015/noslogotipo.png?itok=9kmnetW7',
   },
   {
     name: 'Accenture',
