2f4756c221aeb36907d1d2bcd2dd8e349e69f6ae
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2023-11-16 21:34:18 +0000

    refactor(layouts): student layout

---
+++ _pages/layouts/StudentLayout.tsx
@@ -0,0 +1,20 @@
+import AuthGuard from '@/components/AuthGuard'
+import { UserType } from '@prisma/client'
+import type { PropsWithChildren } from 'react'
+import { NavbarLayout } from '@/_pages/layouts'
+
+type StudentLayoutProps = PropsWithChildren
+
+export const StudentLayout = ({ children }: StudentLayoutProps) => {
+  return (
+    <AuthGuard roles={[UserType.STUDENT, UserType.STAFF]}>
+      <NavbarLayout startTransparent={false}>
+        <div className="h-screen pt-14 sm:pt-20 sm:pb-3 md:px-4 bg-[color:var(--mantine-color-gray-1)]">
+          {children}
+        </div>
+      </NavbarLayout>
+    </AuthGuard>
+  )
+}
+
+export type { StudentLayoutProps }
