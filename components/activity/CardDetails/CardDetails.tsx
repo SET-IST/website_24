4b52728761f680b055d4e229fde09c091fde8aa1
Afonso Pires <107490044+afonsopires1@users.noreply.github.com>
2023-11-26 16:00:29 +0000

    Add files via upload

--- components/activity/CardDetails/CardDetails.tsx
+++ components/activity/CardDetails/CardDetails.tsx
@@ -8,8 +8,8 @@ const CardDetails = ({ title, children }: CardDetailsProps) => {
   return (
     <div className="grow flex flex-col justify-center gap-1 sm:gap-4 md:gap-8 px-4">
       <div className="flex flex-row items-center gap-4">
-        <div className="h-10 sm:h-full w-[2px] bg-secondary-500"></div>
-        <h2 className="py-4 text-2xl md:text-3xl font-light text-secondary-500">
+        <div className="h-10 sm:h-full w-[2px] bg-primary-500"></div>
+        <h2 className="py-4 text-2xl md:text-3xl font-sans font-semibold text-primary-500">
           {title}
         </h2>
       </div>
@@ -20,3 +20,7 @@ const CardDetails = ({ title, children }: CardDetailsProps) => {
 
 export default CardDetails
 export type { CardDetailsProps }
+
+
+
+//className="p-10 text-center text-primary-500 text-4xl font-sans font-semibold"
