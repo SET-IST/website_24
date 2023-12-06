ccde25e406bf73008ef015d10b643ff3adf7ea63
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2023-12-06 21:03:24 +0000

    chore: remove old code

--- pages/api/auth/[...nextauth].ts
+++
@@ -1,7 +0,0 @@
-import NextAuth from 'next-auth'
-import { NextApiRequest, NextApiResponse } from 'next'
-import { AuthOptions } from '@/lib/server/auth'
-
-export default async function auth(req: NextApiRequest, res: NextApiResponse) {
-  return await NextAuth(req, res, AuthOptions(req, res))
-}
