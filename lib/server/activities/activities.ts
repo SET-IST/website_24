2d73ab9e71bbef044dd747490cb9b29948412d22
Miguel Vale <miguel@DESKTOP-CVOPEMS>
2023-12-12 11:07:23 +0000

    feat(lib/server/activities) implemented two features: enrroll a student in an activity and remove a student from an activity

--- lib/server/activities/activities.ts
+++ lib/server/activities/activities.ts
@@ -1,8 +1,10 @@
 import { databaseQueryWrapper } from '@/core/utils'
 import { PrismaService } from '../../../core/services/server'
-import { Activity, UserType } from '@prisma/client'
+import { Activity, ActivityType, UserType } from '@prisma/client'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from '@/core/middlewares/server/require-session'
+import { User } from '@/components/pages/PerfilPage/types'
+import { BadRequestException, ConflictException } from 'next-api-decorators'
 
 export async function getActivities(
   req: NextApiRequest,
@@ -36,5 +38,90 @@ export async function getActivities(
       }
       return activity
     })
+  })
+}
+
+export async function enrollStudent(user: User, id: number) {
+  return await databaseQueryWrapper(async () => {
+
+    const activity = await PrismaService.activity.findUniqueOrThrow({
+      where: {
+        id: id,
+      },
+    })
+
+    if (activity.type === ActivityType.Lecture)
+      throw new BadRequestException('Cannot enroll in a lecture')
+
+    const student = await PrismaService.studentDetails.findUniqueOrThrow({
+      where: {
+        userId: user.id,
+      },
+      include: {
+        enrolledActivities: true,
+      },
+    })
+
+    const enrolledActivity = student.enrolledActivities.find(
+      (enrolledActivity) => enrolledActivity.activityId == activity.id
+    )
+
+    if (!enrolledActivity){
+      await PrismaService.activityEnrollment.create({
+        data: {
+          student: {
+            connect: {
+              id: student.id,
+            },
+          },
+          activity: {
+            connect: {
+              id: activity.id,
+            },
+          },
+          confirmed: false,
+        },
+      })
+      return {message: 'Successfully enrolled'}
+    }
+    else return {message: 'Already enrolled'}
+  })
+}
+
+export async function removeStudent(user: User, id: number) {
+  return await databaseQueryWrapper(async () => {
+
+    const activity = await PrismaService.activity.findUniqueOrThrow({
+      where: {
+        id: id,
+      },
+    })
+
+    if (activity.type === ActivityType.Lecture)
+      throw new BadRequestException('Cannot enroll in a lecture')
+
+    const student = await PrismaService.studentDetails.findUniqueOrThrow({
+      where: {
+        userId: user.id,
+      },
+      include: {
+        enrolledActivities: true,
+      },
+    })
+
+    const enrolledActivity = student.enrolledActivities.find(
+      (enrolledActivity) => enrolledActivity.activityId == activity.id
+    )
+
+    if (student.id == enrolledActivity?.studentId) {
+      await PrismaService.activityEnrollment.delete({
+        where: {
+          studentId: enrolledActivity.studentId,
+          activityId: enrolledActivity.activityId,
+        },
+      })
+      return {message: 'Successfully removed'}
+    }
+    else throw new BadRequestException('Student not enrolled')
   })
 }
