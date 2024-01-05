cb986e5e130a01acd63d2fedb1aa619c2987544e
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2024-01-05 01:00:14 +0000

    feat(pages/api/auth): configure Credentials authentication

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,75 +1,76 @@
-import { FenixAccessTokenHandler, FenixProfileHandler } from '@/core/auth'
-import GoogleProvider from 'next-auth/providers/google'
 import NextAuth from 'next-auth'
+
+// Providers
+import GoogleProvider from 'next-auth/providers/google'
+import Credentials from 'next-auth/providers/credentials'
+
+// Handlers and Callbacks
+import {
+  AuthCallbacks,
+  CredentialsAuthorizationHandler,
+  FenixAccessTokenHandler,
+  FenixProfileHandler,
+  GoogleProfileHandler,
+  JWTHandlers,
+} from '@/lib/server/auth'
+
+// Database adapter
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { PrismaService } from '@/core/services/server'
 import { Adapter } from 'next-auth/adapters'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { links } from '@/data/links'
 
-export default NextAuth({
-  adapter: PrismaAdapter(PrismaService) as Adapter,
-  providers: [
-    {
-      id: 'fenix',
-      name: 'Fénix',
-      type: 'oauth',
-      version: '2.0',
-      clientId: process.env.FENIX_ID!,
-      clientSecret: process.env.FENIX_SECRET!,
-      token: FenixAccessTokenHandler,
-      authorization: {
-        url: `https://fenix.tecnico.ulisboa.pt/oauth/userdialog`,
-      },
-      userinfo: {
-        url: 'https://fenix.tecnico.ulisboa.pt/api/fenix/v1/person',
-      },
-      profile: FenixProfileHandler,
-    },
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      authorization: {
-        params: {
-          prompt: 'consent',
-          access_type: 'offline',
-          response_type: 'code',
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, {
+    adapter: PrismaAdapter(PrismaService) as Adapter,
+    providers: [
+      {
+        id: 'fenix',
+        name: 'Fénix',
+        type: 'oauth',
+        version: '2.0',
+        clientId: process.env.FENIX_ID!,
+        clientSecret: process.env.FENIX_SECRET!,
+        token: FenixAccessTokenHandler,
+        authorization: {
+          url: `https://fenix.tecnico.ulisboa.pt/oauth/userdialog`,
         },
+        userinfo: {
+          url: 'https://fenix.tecnico.ulisboa.pt/api/fenix/v1/person',
+        },
+        profile: FenixProfileHandler,
       },
-      profile(profile) {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-          studentDetails: {
-            create: {
-              university: '',
-              course: '',
-            },
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        authorization: {
+          params: {
+            prompt: 'consent',
+            access_type: 'offline',
+            response_type: 'code',
           },
+        },
+        profile: GoogleProfileHandler,
+      }),
+      Credentials({
+        type: 'credentials',
+        credentials: {},
+        authorize: CredentialsAuthorizationHandler,
+      }),
+    ],
+    events: {
+      signIn({ account, isNewUser }) {
+        if (account?.provider === 'google' && isNewUser) {
+          // Redirect new external student
         }
       },
-    }),
-  ],
-  events: {
-    signIn({ account, isNewUser }) {
-      if (account?.provider === 'google' && isNewUser) {
-        // Redirect new external student
-      }
     },
-  },
-  callbacks: {
-    async session({ session, token, user }) {
-      session.user = {
-        id: user.id,
-        role: user.role,
-        readChangelog: user.readChangelog,
-        ...session.user,
-      }
-
-      return session
+    callbacks: AuthCallbacks(req, res),
+    jwt: JWTHandlers(req, res),
+    pages: {
+      error: '/',
+      signIn: '/',
     },
-  },
-  pages: {
-    error: '/',
-    signIn: '/',
-  },
-})
+  })
+}
