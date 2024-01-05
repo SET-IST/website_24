519f20c4041d398547d65632746dbac369097187
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2024-01-05 01:09:43 +0000

    chore(lib/server/auth): add comment to jwt module

--- lib/server/auth/jwt.ts
+++ lib/server/auth/jwt.ts
@@ -2,6 +2,24 @@ import Cookies from 'cookies'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { JWTOptions, decode, encode } from 'next-auth/jwt'
 
+/* 
+
+Wait, JWT? Do we use this? No
+
+NextAuth apparently discourages users from creating their own user 
+store and using password based authentication as a main route as well 
+as the inherent complications with securing and supporting password validation.
+
+How to bypass this?
+
+We must manipulate the output from the JWT encode to not returned the JWT signed 
+tokens that would encapsulate the user data but instead return the sessionToken 
+value instead that references our database sessions table.
+
+https://github.com/nextauthjs/next-auth/discussions/4394
+
+*/
+
 export const JWTHandlers = (
   req: NextApiRequest,
   res: NextApiResponse
