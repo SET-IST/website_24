4f71e71f9143f6e3b12c8c7a615af525975e150c
Miguel Vale <msvv2014@gmail.com>
2024-01-27 19:12:42 +0000

    fix(lib/frontend) StudentCompanyScan type

--- lib/frontend/api/student.ts
+++ lib/frontend/api/student.ts
@@ -7,13 +7,15 @@ import {
 } from '@/lib/server/services/student'
 import { FileWithPath } from '@mantine/dropzone'
 
+type Unpacked<T> = T extends (infer U)[] ? U : T
+
 // Get types from backend services
 export type StudentProfile = Awaited<ReturnType<typeof getStudentProfile>>
 export type StudentProfilePatchResponse = Awaited<
   ReturnType<typeof patchStudentProfile>
 >
 
-export type StudentCompanyScans = Awaited<ReturnType<typeof getStudentCompanies>>
+export type StudentCompanyScan = Unpacked<Awaited<ReturnType<typeof getStudentCompanies>>>
 
 export const fetchStudentProfile = async (): Promise<StudentProfile> => {
   const { data } = await ApiClient.get('student/profile')
@@ -28,7 +30,7 @@ export const updateStudentProfile = async (
   return profile
 }
 
-export const fetchStudentCompaniesScans = async (): Promise<StudentCompanyScans> => {
+export const fetchStudentCompaniesScans = async (): Promise<StudentCompanyScan[]> => {
   const { data } = await ApiClient.get('student/companies')
   return data
 }
