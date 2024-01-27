f6816ee320b288f378f6ecdedc8ea42bed549e74
Miguel Vale <msvv2014@gmail.com>
2024-01-27 18:44:04 +0000

    fix(lib/frontend) change the CompanyStudentScan type

--- lib/frontend/api/company.ts
+++ lib/frontend/api/company.ts
@@ -6,13 +6,15 @@ import {
 } from '@/lib/server/services/company'
 import { IPatchCompanyProfile } from '@/lib/server/services/company/dtos'
 
+type Unpacked<T> = T extends (infer U)[] ? U : T
+
 // Get types from backend services
 export type CompanyProfile = Awaited<ReturnType<typeof getCompanyProfile>>
 export type CompanyProfilePatchResponse = Awaited<
   ReturnType<typeof patchCompanyProfile>
 >
 
-export type CompanyStudentScans = Awaited<ReturnType<typeof getCompanyStudents>>
+export type CompanyStudentScan = Unpacked<Awaited<ReturnType<typeof getCompanyStudents>>>
 
 export const fetchCompanyProfile = async (): Promise<CompanyProfile> => {
   const { data } = await ApiClient.get('company/profile')
@@ -27,7 +29,7 @@ export const updateCompanyProfile = async (
   return profile
 }
 
-export const fetchCompanyStudentsScans = async (): Promise<CompanyStudentScans> => {
+export const fetchCompanyStudentsScans = async (): Promise<CompanyStudentScan[]> => {
   const { data } = await ApiClient.get('company/students')
   return data
 }
