7884e25a4115268b54dffc0b4e14c079ae4ad110
Miguel Vale <msvv2014@gmail.com>
2024-01-27 18:45:35 +0000

    feat(_pages/ProfilePage/components/UserTabs/Students) use the hook in the students UserTab

--- _pages/ProfilePage/components/UserTabs/Students/Student.tsx
+++ _pages/ProfilePage/components/UserTabs/Students/Student.tsx
@@ -1,33 +1,28 @@
+import { CompanyStudentScan } from '@/lib/frontend/api'
+import { getCourse, getInstitution } from '@/lib/frontend/utils'
 import { Avatar, Button, Text } from '@mantine/core'
 import { Image } from '@mantine/core'
 import { IconDownload, IconFileDownload } from '@tabler/icons-react'
 
-export interface StudentData {
-  id: string
-  name: string
-  university: string
-  course: string
-  img: string
-}
 
 interface StudentComponentProps {
-  data: StudentData
+  data: CompanyStudentScan
 }
 
 const Student = ({ data }: StudentComponentProps) => {
   return (
     <div className="w-full h-fit p-4 flex flex-row items-center justify-between hover:bg-gray-50 transition-all">
       <div className="flex flex-row items-center">
-        <Avatar src={data.img} h={52} w={52} />
+        <Avatar src={data?.user.image} h={52} w={52} />
         <div className="ml-4 sm:ml-6 flex flex-col items-start">
           <Text c="#00415a" fw={600}>
-            <span className="text-lg">{data.name}</span>
+            <span className="text-lg">{data?.user.name}</span>
           </Text>
           <Text c="dimmed" lh={1} lineClamp={2} fw={500}>
-            <span className="text-sm">{data.university}</span>
+            <span className="text-sm">{getInstitution(data?.university)?.name}</span>
           </Text>
           <Text c="dimmed" lh={1} lineClamp={2} fw={500}>
-            <span className="text-sm">{data.course}</span>
+            <span className="text-sm">{getCourse(data?.university,data?.course)?.name}</span>
           </Text>
         </div>
       </div>
