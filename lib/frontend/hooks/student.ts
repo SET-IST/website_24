7f33acd299dc389c2e55d3b85e35407c385ce8ee
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2024-01-28 19:37:34 +0000

    chore(lib/frontend/hooks): create useScan hook

--- lib/frontend/hooks/student.ts
+++ lib/frontend/hooks/student.ts
@@ -1,12 +1,33 @@
-import { useQuery } from "@tanstack/react-query";
-import { useSession } from "next-auth/react";
-import { StudentCompanyScan, fetchStudentCompaniesScans } from "../api";
-import { UserType } from "@prisma/client";
+import { QueryClient, useMutation, useQuery } from '@tanstack/react-query'
+import { useSession } from 'next-auth/react'
+import {
+  CompanyScan,
+  StudentCompanyScan,
+  fetchStudentCompaniesScans,
+  scanCompany,
+} from '../api'
+import { UserType } from '@prisma/client'
+import { AxiosError } from 'axios'
 
 export const useStudentCompanyScans = () => {
-    const session = useSession();
-    const user = session.data?.user;
-    return useQuery<StudentCompanyScan[], Error>(['StudentCompanyScans'], () => fetchStudentCompaniesScans(), {
-        enabled: session.status === 'authenticated' && (user?.role === UserType.Student || user?.role === UserType.Staff)
-    });
+  const session = useSession()
+  const user = session.data?.user
+  return useQuery<StudentCompanyScan[], AxiosError>(
+    ['StudentCompanyScans'],
+    () => fetchStudentCompaniesScans(),
+    {
+      enabled:
+        session.status === 'authenticated' &&
+        (user?.role === UserType.Student || user?.role === UserType.Staff),
+    }
+  )
+}
+
+export const useScan = (queryClient: QueryClient) => {
+  return useMutation<CompanyScan, AxiosError, string>({
+    mutationFn: async (companyId: string) => {
+      return scanCompany(companyId)
+    },
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['Profile'] }),
+  })
 }
