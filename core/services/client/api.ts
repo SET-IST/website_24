ab19b449290a3f856f589e881e88946fe472f4a2
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2024-02-15 23:53:31 +0000

    fix(api): add client logging

--- core/services/client/api.ts
+++ core/services/client/api.ts
@@ -4,4 +4,8 @@ const ApiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 })
 
+console.log(
+  `[ApiClient]: Initialized with path: ${process.env.NEXT_PUBLIC_API_BASE_URL}`
+)
+
 export { ApiClient }
