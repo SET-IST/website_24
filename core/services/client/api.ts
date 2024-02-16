93727f3d0f4edf4a3ba484803d7f172765003fdd
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2024-02-16 03:37:13 +0000

    chore(api): remove client debug logging

--- core/services/client/api.ts
+++ core/services/client/api.ts
@@ -4,8 +4,4 @@ const ApiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 })
 
-console.log(
-  `[ApiClient]: Initialized with path: ${process.env.NEXT_PUBLIC_API_BASE_URL}`
-)
-
 export { ApiClient }
